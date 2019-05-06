38329232b5a5471579ec8ca4004c03cfaa350bdb
Madimetja Sethosa <madimetjasethosa@gmail.com>
2019-05-06 23:44:34 +0200

    MPI file right fn

---
+++ MPI_HELLO_WORLD/MPIwrite.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{ 
+
+	int rank, i; char a[10]; char mesg[100];
+	
+	
+	MPI_Offset n = 10; MPI_File fh ; MPI_Status status ; 
+
+	MPI_Init(&argc, &argv); 
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+	if(rank!=0)
+	{
+		sprintf(mesg, "Greetings from %d!",rank);
+		printf("%s\n", mesg);
+	}
+	
+	for (i=0; i<10; i++)
+		a[i] = (char)( '0' + rank);  // e.g. on processor 3 creates a[0:9]=’3333333333’ 
+
+	MPI_File_open (MPI_COMM_WORLD, "data.out", MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &fh); 
+	MPI_Offset displace = rank*n*sizeof(char); // start of the view for each processor 
+
+	MPI_File_set_view (fh , displace , MPI_CHAR, MPI_CHAR, "native" ,MPI_INFO_NULL);
+	// note that etype and filetype are the same 
+
+	MPI_File_write(fh, a, n, MPI_CHAR, &status);
+	 
+	MPI_File_close(&fh ) ; 
+
+	MPI_Finalize ( ) ; 
+
+	return 0;
+	
+}
