276fc26953898e9ca9582a23adf161cf92e115be
Madimetja Sethosa <madimetjasethosa@gmail.com>
2019-05-10 19:18:29 +0200

    created a MPI read test

--- MPI_HELLO_WORLD/testfile.c
+++ MPI_HELLO_WORLD/testfile.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 		a[i] = (char)( '0' + rank);  // e.g. on processor 3 creates a[0:9]=’3333333333’ 
 
 	 
-	MPI_Offset displace = rank*n*sizeof(int); // start of the view for each processor 
+	MPI_Offset displace = rank*n*sizeof(char); // start of the view for each processor 
 	
 	
 	MPI_File_open (MPI_COMM_WORLD, "data.txt", MPI_MODE_CREATE|MPI_MODE_RDWR , MPI_INFO_NULL, &fh);
@@ -35,12 +35,12 @@ int main(int argc, char **argv)
 	// note that etype and filetype are the same 
 
 	MPI_File_write(fh, a, n, MPI_CHAR, &status);
-	/*
-	MPI_File_set_view (fh , 10 , MPI_CHAR, MPI_CHAR, "native" ,MPI_INFO_NULL);
+	
+	MPI_File_set_view (fh , displace , MPI_CHAR, MPI_CHAR, "native" ,MPI_INFO_NULL);
 	// note that etype and filetype are the same 
 	
-	MPI_File_read (fh, &b, 2, MPI_CHAR, &status);
-	printf("printing from %d\n, we have : %s\n",rank,b);*/
+	MPI_File_read (fh, b, n, MPI_CHAR, &status);
+	printf("printing from %d\n, we have : %s\n",rank,b);
 	MPI_File_close(&fh ) ; 
 
 	MPI_Finalize ( ) ; 
